7d176f0bf054ac3c1057754d94266c28e3188319
unknown <aromdanelli@hotmail.com>
2023-10-15 00:12:00 -0300

    Crud finished. Next i need to make some finishing touches

--- lib/views/user-form.dart
+++ lib/views/user-form.dart
@@ -7,17 +7,23 @@ class UserForm extends StatelessWidget {
   final _form = GlobalKey<FormState>();
   final Map<String, String> _formData = {};
 
-  void _loadFormData(User user){
-    _formData['id'] = user.id;
-
+  void _loadFormData(User? user) { // Temos 2 páginas no nosso app, a primeira e a de cadastro, correto? Quando clicamos em cadastrar um novo usuario, as informações das fields vão estar vazias.
+    if (user != null) {
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarUrl'] = user.avatarUrl;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final user = ModalRoute.of(context)?.settings.arguments as User? ;//se tirar o ! ele xia
+    final user = ModalRoute.of(context)?.settings.arguments
+        as User?; //se tirar o ! ele xia
 
-    //_loadFormData();
-
+    if (user != null) {
+      _loadFormData(user); // ai como to chamando ele aqui pra dentro do app, precisa chamar um if pra ter ctz q o usuario n é null pq se for essa porra n funciona e é isso ai
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de Usuário'),
@@ -25,7 +31,7 @@ class UserForm extends StatelessWidget {
           IconButton(
               icon: const Icon(Icons.save),
               onPressed: () {
-                _form.currentState?.save(); // se tirar o exclamação ele xia
+                _form.currentState?.save(); // se tirar o interrogação ele xia
 
                 String id = _formData['id'] ?? '';
                 String name = _formData['name'] ?? '';
@@ -46,14 +52,17 @@ class UserForm extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
+                initialValue: _formData['name'],
                 decoration: const InputDecoration(labelText: 'Nome'),
                 onSaved: (newValue) => _formData['name'] = newValue!,
               ),
               TextFormField(
+                initialValue: _formData['email'],
                 decoration: const InputDecoration(labelText: 'Email'),
                 onSaved: (newValue) => _formData['email'] = newValue!,
               ),
               TextFormField(
+                initialValue: _formData['avatarUrl'],
                 decoration: const InputDecoration(labelText: 'URL do Avatar'),
                 onSaved: (newValue) => _formData['avatarUrl'] = newValue!,
               ),
